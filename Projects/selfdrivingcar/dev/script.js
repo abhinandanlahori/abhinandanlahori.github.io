7b21039983d25d14a9bb51245ee05220ef694b2b
Abhinandan Lahori <abhinandanlahori@gmail.com>
2023-08-13 01:26:07 +0530

    Added all projects

---
+++ Projects/selfdrivingcar/dev/script.js
@@ -0,0 +1,17 @@
+myCanvas.height=window.innerHeight;
+myCanvas.width=200;
+
+const ctx=myCanvas.getContext("2d");
+const road= new Road(myCanvas.width/2,myCanvas.width*0.9);
+const car=new Car(road.getLaneCenter(1),100,30,50);
+
+animate();
+
+function animate(){
+    car.update();
+    myCanvas.height=window.innerHeight;
+    ctx.translate(0,-car.y+myCanvas.height*0.7);
+    road.draw(ctx);
+    car.draw(ctx);
+    requestAnimationFrame(animate);
+}
